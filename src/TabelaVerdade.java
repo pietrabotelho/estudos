90cb5da6ede01ce8035377e60a39e3812f3643c4
pietrabotelho <pietrw.botelho@gmail.com>
2023-05-31 13:21:53 -0300

    primeiro commit

---
+++ src/TabelaVerdade.java
@@ -0,0 +1,12 @@
+/* Tente isto 2-2: Tabela verdade para os operadores lógicos.
+Colunas alinhadas com \t, embutindo tabulações em cada string de saída.
+ */
+
+public class TabelaVerdade {
+    public static void main(String[] args) {
+
+        boolean p, q;
+
+        System.out.println("P\tQ\tAND\tOR\tXOR\tNOT");
+    }
+}
